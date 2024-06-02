e2039d324d245baa75d74305cf67cf6df3e0f894
hanzsdev <149425914+hanzsdev@users.noreply.github.comm>
2024-06-02 15:35:15 +0100

    Refactor opacity function and change css gradient

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,6 +1,5 @@
 function userPrompt() {
     const button = document.querySelector('.button');
-    console.log(button);
 
     button.addEventListener('click', () => {
         let userInput = prompt('Choose a grid size', '');
@@ -14,10 +13,13 @@ function userPrompt() {
     });
 };
 
+userPrompt();
+
 function grid(value) {
     const cellSize = 640 / value 
 
     const container = document.querySelector('.main-grid-container');
+    container.innerHTML = '';
 
     for(let i = 0; i < value; i++) {
         const rows = document.createElement('div');
@@ -31,6 +33,34 @@ function grid(value) {
                 rows.appendChild(columns);
             }
     }
+
+    gridSelection();
 };
 
 grid(16);
+
+function randomHex() {
+    const hexiDecimal = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+    let hex = '#';
+
+    for (let i = 0; i < 6; i++) {
+        const randomindex = Math.floor(Math.random()* hexiDecimal.length);
+        hex += hexiDecimal[randomindex];
+    }
+    return hex;
+};
+
+function gridSelection() {
+    const grid = document.querySelectorAll('.divColumns');
+
+    grid.forEach(item => {
+        item.addEventListener('mouseover', () => {
+            const randomColor = randomHex();
+            item.style.backgroundColor = randomColor;
+            let currentOapcity = parseFloat(item.style.opacity);
+            currentOapcity = isNaN(currentOapcity) ? 1 : currentOapcity;
+            const newOapcity = Math.max(currentOapcity - 0.1,0);
+            item.style.opacity = newOapcity;
+        });
+    });
+};
